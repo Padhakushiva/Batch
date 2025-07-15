3adb930935179f9bd8cafaff98c79f3b9a8186d3
Shiva Choudhry <145893913+Padhakushiva@users.noreply.github.com>
2025-07-15 11:50:15 +0530

    Create combined_task.js

---
+++ combined_task.js
@@ -0,0 +1,21 @@
+// Show greeting
+alert("Welcome, user!");
+
+// Arrow function
+const add = (a, b) => a + b;
+
+// Test the function
+console.log("Add Results:");
+console.log(add(10, 5));
+console.log(add(2, 3));
+
+// typeof examples
+let city = "Delhi";
+let temp = 30;
+console.log("Types:");
+console.log(typeof city); // string
+console.log(typeof temp); // number
+
+// Log window object
+console.log("Window object:");
+console.log(window);
